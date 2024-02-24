09e06890941638949e652e0c9dcde319e7e6d528
Junaid Mansur Ifti <bsse1027@iit.du.ac.bd>
2024-02-25 01:18:42 +0600

    Init commit

---
+++ DFS/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DFS;
+
+public class Solution
+{
+    public void Dfs(TreeNode root, int targetSum, int currentSum, ref bool ans)
+    {
+        if(root == null)
+        {
+            ans = false;
+            return;
+        }
+        if(ans == true)
+        {
+            return;
+        }
+        currentSum += root.val;
+        if (root.left == null && root.right == null && currentSum == targetSum)
+        {
+            ans = true;
+            return;
+        }
+        if(root.left != null)
+        {
+            Dfs(root.left, targetSum, currentSum, ref ans);
+        }
+        if(root.right != null)
+        {
+            Dfs(root.right, targetSum, currentSum, ref ans);
+        }
+    }
+    public bool HasPathSum(TreeNode root, int targetSum)
+    {
+        var ans = false;
+        Dfs(root, targetSum, 0, ref ans);
+        return ans;
+    }
+}
