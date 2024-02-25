06d81599421280f4ff4fa024d32c13a230d1b339
Junaid Mansur Ifti <bsse1027@iit.du.ac.bd>
2024-02-25 15:36:44 +0600

    24th Feb

--- TestClass/Class1.cs
+++ TestClass/Class1.cs
@@ -1,5 +1,30 @@
 ﻿namespace TestClass
 {
+    public class ListNode
+    {
+      public int val;
+      public ListNode? next;
+      public ListNode(int val = 0, ListNode? next = null)
+      {
+        this.val = val;
+        this.next = next;
+      }
+    }
+
+    public class Solution
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            ListNode first = null, second = null, third = null;
+            if(head != null && head.next != null)
+            {
+                first = head;
+                second = head.next;
+                third = second.next;
+            }
+        }
+    }
+
     public class Class1
     {
 
